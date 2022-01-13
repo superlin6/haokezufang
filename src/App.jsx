d539a6c4c3c5290992b23f4b22ac27442197e4a2
coderlin <coderlin@zego.im>
2022-01-13 16:54:35 +0800

    feat :项目搭建 + tabbar

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { TabBar } from "antd-mobile";
+import React from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import CityList from "./pages/CityList";
+import Home from "./pages/Home";
+
+import {
+  AppOutline,
+  MessageOutline,
+  UnorderedListOutline,
+  UserOutline,
+} from "antd-mobile-icons";
+import "./App.scss";
+
+export default function App() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const tabs = [
+    {
+      key: "/home",
+      title: "首页",
+      icon: <AppOutline />,
+    },
+    {
+      key: "/citylist",
+      title: "列表",
+      icon: <UnorderedListOutline />,
+    },
+    {
+      key: "/message",
+      title: "资讯",
+      icon: <MessageOutline />,
+    },
+    {
+      key: "/me",
+      title: "我的",
+      icon: <UserOutline />,
+    },
+  ];
+
+  function togglePath(path) {
+    navigate(path);
+  }
+  return (
+    <div className="App">
+      <div className="app-container">
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/citylist" element={<CityList />}></Route>
+        </Routes>
+      </div>
+      <TabBar
+        className="tab-bar"
+        activeKey={pathname}
+        onChange={(path) => togglePath(path)}
+      >
+        {tabs.map((item) => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
+    </div>
+  );
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+export default function Home() {
+    return (
+        <div className="home">
+            home
+        </div>
+    )
+}
